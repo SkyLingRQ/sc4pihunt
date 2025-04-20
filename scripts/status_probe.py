8acd9e5b62eff95bfccc7de128ed55f102e8947b
SkyLing <70288759+SkyLingRQ@users.noreply.github.com>
2025-04-20 20:42:15 -0300

    Update status_probe.py

--- scripts/status_probe.py
+++ scripts/status_probe.py
@@ -3,17 +3,24 @@ import aiohttp
 from colorama import init, Fore
 
 init()
+rd = Fore.RED
 g = Fore.GREEN
+yellow = Fore.YELLOW
 reset = Fore.RESET
+
+urlLive = []
 
 async def status_probe(url, session):
     try:
         url = url.strip()
+        if not url.startswith(('http://', 'https://')):
+            url = 'http://' + url
         async with session.get(url) as url_response:
             if url_response.status == 200:
                 print(f"{g}[200] {url}{reset}")
+                urlLive.append(url)
             else:
-                pass
+                print(f"{yellow}[{url_response.status}] {url}{reset}")
     except Exception:
         pass
 
@@ -27,3 +34,9 @@ async def main(file):
     async with aiohttp.ClientSession() as session:
         task = [status_probe(url, session) for url in urls]
         await asyncio.gather(*task)
+    
+
+    if urlLive:
+        with open("subdomains_live.txt", 'a') as live:
+            for urll in urlLive:
+                live.write(urll+"\n")
