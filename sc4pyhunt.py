11366fa5b9f325dd9a455898bafe55bd2cdb78e3
SkyLing <70288759+SkyLingRQ@users.noreply.github.com>
2025-04-20 20:43:04 -0300

    Update sc4pyhunt.py

--- sc4pyhunt.py
+++ sc4pyhunt.py
@@ -7,13 +7,13 @@ parse.add_argument("-sP", "--status", help="Realiza un escaneo de estado HTTP (s
 parse.add_argument("-cx", "--cors", help="Detecta posibles vulnerabilidades de CORS en las URLs proporcionadas.")
 parse.add_argument("-op", "--openredirect", help="Escanea las URLs en busca de vulnerabilidades de redirección abierta.")
 parse.add_argument("--xss", help="Prueba vulnerabilidades de Cross-Site Scripting (XSS) en las URLs.")
-#parse.add_argument("-api", "-api-endpoints", help="Busca endpoints de API en las URLs objetivo.")
+parse.add_argument("-api", "-api-endpoints", help="Busca endpoints de API en las URLs objetivo.")
 parse.add_argument("-S", "--subdomains", help="Enumera subdominios de un dominio dado.")
 parse.add_argument("-wu", "--wayback-url", help="Recupera URLs archivadas para un dominio usando Wayback Machine.")
 parse.add_argument("-WU", "--wayback-urls", action="store_true", help="Recupera y lista múltiples URLs archivadas de Wayback Machine.")
 parse.add_argument("-ej", "--extrack-js", help="Extrae archivos JavaScript desde la URL proporcionada.")
 parse.add_argument("-jS", "--jsensitive", help="Escanea archivos JavaScript en busca de datos sensibles (tokens, claves, etc).")
-parse.add_argument("-pT", "--pathTraversal", help="Detecta posibles fallos de path traversal en parametros mediante una lista de payloads.")
+#parse.add_argument("-pT", "--pathTraversal", help="Detecta posibles fallos de path traversal en parametros mediante una lista de payloads.")
 
 args = parse.parse_args()
 
@@ -31,9 +31,9 @@ if args.xss:
     from scripts.vacpxss import main as vacpxss
     asyncio.run(vacpxss(args.xss))
 if args.subdomains:
-    os.system(f"sudo subfinder -d {args.subdomains} -all -recursive -silent -o s.txt && sudo assetfinder -subs-only {args.subdomains} > e.txt && sort -u s.txt e.txt > subdomains.txt && sudo rm -rf e.txt s.txt")
+    os.system(f"subfinder -d {args.subdomains} -all -recursive -silent -o s.txt && assetfinder -subs-only {args.subdomains} > e.txt && sort -u s.txt e.txt > subdomains.txt && rm -rf e.txt s.txt")
 if args.wayback_url:
-    os.system(f"waybackurls {args.wayback_url} >> s.txt && gau {args.wayback_url} >> t.txt && sort -u s.txt t.txt > pathone.txt && sudo rm -rf s.txt t.txt")
+    os.system(f"waybackurls {args.wayback_url} >> s.txt && gau {args.wayback_url} >> t.txt && sort -u s.txt t.txt > pathone.txt && rm -rf s.txt t.txt")
 if args.wayback_urls:
     archive_path = input("[ FILE ] Path: ")
     output_file = "resultados.txt"
@@ -43,11 +43,15 @@ if args.wayback_urls:
         for url in urls:
             url = url.strip()
             os.system(f"waybackurls {url} >> {output_file}")
+            os.system(f"cat {output_file} | uniq > {output_file}")
 if args.extrack_js:
     os.system(f"katana -u {args.extrack_js} -silent -em js -o jsfiles.txt")
 if args.jsensitive:
     from scripts.jsensitive import main as jsensitive
     asyncio.run(jsensitive(args.jsensitive))
-if args.pathTraversal:
-    from scripts.directory_traversal import main as pathtraversal
-    asyncio.run(pathtraversal(args.pathTraversal))
+#if args.pathTraversal:
+#    from scripts.directory_traversal import main as pathtraversal
+#    asyncio.run(pathtraversal(args.pathTraversal))
+if args.api:
+    from scripts.api_endpoints import main as fuzzing_api
+    asyncio.run(fuzzing_api(args.api))
